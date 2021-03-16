5087212010962ea5e06cbaf1fc148eb431953692
sandrossano <32746792+sandrossano@users.noreply.github.com>
2021-03-16 11:08:25 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -160,7 +160,7 @@ class App extends Component {
 
       var http = new XMLHttpRequest();
       var url =
-        "https://cors-casillo-sap.herokuapp.com/https://sap.casillogroup.it/sap/opu/odata/sap/ZWEB_ALLEGATI_SRV/ddtAllegatiSet";
+        "https://cors-casillo-sap.herokuapp.com/https://sap.casillogroup.it:1080/sap/opu/odata/sap/ZWEB_ALLEGATI_SRV/ddtAllegatiSet";
       var body =
         '<entry xml:base="/sap/opu/odata/sap/ZWEB_USERS_SRV/" xmlns="http://www.w3.org/2005/Atom" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata" ' +
         'xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices">' +
@@ -289,7 +289,7 @@ class App extends Component {
                   className={css.ImageLabel}
                   width="40px"
                   alt="Icona Cam"
-                  src="fotocam.ico"
+                  src="folderPicker.ico"
                 />
               </p>
               <input
